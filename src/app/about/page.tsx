e403455a4778111fbedf9d11c57aaad157cc700f
ionutbirlad <birladionut@yahoo.it>
2026-05-11 17:06:57 +0200

    feat: add missing style modules + adapt remaining pages

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,13 @@
-const About = () => {
-  return <div>ABOUT PAGE HERE</div>;
-};
+import styles from "./page.module.scss";
 
-export default About;
+export default function About() {
+  return (
+    <div className={styles.page}>
+      <section>
+        <div className="container">
+          <div className={styles.page__inner}>ABOUT PAGE HERE</div>
+        </div>
+      </section>
+    </div>
+  );
+}
