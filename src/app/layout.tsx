1d999d608509749af971f4d2f9d9ad0aa9684b21
ionutbirlad <birladionut@yahoo.it>
2026-05-18 16:45:31 +0200

    reafctor: use index module as interface for exporting AppFooter and AppHeader

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter, Noto_Serif, Roboto_Mono } from "next/font/google";
 
-import AppFooter from "@/components/AppFooter/AppFooter";
-import AppHeader from "@/components/AppHeader/AppHeader";
+import { AppFooter } from "@/components/AppFooter";
+import { AppHeader } from "@/components/AppHeader";
 
 import "@/styles/index.scss";
 import styles from "./layout.module.scss";
