e1d0314c37df8df268cc1e0c76ea3251d77e3f53
ionutbirlad <birladionut@yahoo.it>
2026-06-09 15:12:52 +0200

    fix: omit original title property + add children prop

--- src/components/ui/AvatarBlock/AvatarBlock.types.ts
+++ src/components/ui/AvatarBlock/AvatarBlock.types.ts
@@ -1,6 +1,7 @@
-import { type HTMLAttributes } from "react";
+import type { HTMLAttributes, ReactNode } from "react";
 
-export type AvatarBlockProps = HTMLAttributes<HTMLDivElement> & {
+export type AvatarBlockProps = Omit<HTMLAttributes<HTMLDivElement>, "title"> & {
   title: string;
   description: string;
+  children: ReactNode;
 };
