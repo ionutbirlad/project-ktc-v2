3e2876f0574d0ec517fb0427a8eb963f1cd29efb
ionutbirlad <birladionut@yahoo.it>
2026-06-09 15:16:15 +0200

    fix: apply minor details for robustness

--- src/components/ui/AvatarBlock/AvatarBlock.stories.tsx
+++ src/components/ui/AvatarBlock/AvatarBlock.stories.tsx
@@ -9,11 +9,7 @@ type AvatarBlockMeta = Meta<typeof AvatarBlock>;
 type Story = StoryObj<typeof AvatarBlock>;
 
 function renderAvatar() {
-  return (
-    <>
-      <Avatar src={AvatarMock} size="large" alt="Avatar mock picture" />
-    </>
-  );
+  return <Avatar src={AvatarMock} size="large" alt="Avatar mock picture" />;
 }
 
 const meta: AvatarBlockMeta = {
@@ -34,6 +30,14 @@ export const Default: Story = {
   args: {
     title: "Title",
     description: "Description",
+  },
+  render: (args) => <AvatarBlock {...args}>{renderAvatar()}</AvatarBlock>,
+};
+
+export const LongText: Story = {
+  args: {
+    title: "Very long avatar block title that should truncate",
+    description: "Very long description that should also truncate correctly",
   },
   render: (args) => <AvatarBlock {...args}>{renderAvatar()}</AvatarBlock>,
 };
